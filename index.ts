c44b75317b86d81d0622363740a50f20f071a079
Mario Patronelli <mar.ioepatronelli@gmail.com>
2022-11-27 22:57:29 -0300

    corregi mensajes de error, placeholder de email, limite maximo en comentario y estado invertido de activacion del button.

--- index.ts
+++ index.ts
@@ -15,6 +15,7 @@ function toggleText() {
 }
 
 //-----------------------------------------------------------------------------
+const maxComentario: number = 250;
 const inputNombre = document.getElementById("contactForm1") as HTMLInputElement;
 const inputEmpresa = document.getElementById("contactForm2") as HTMLInputElement; // deberia ser opcional ?
 const inputEmail = document.getElementById("contactForm3") as HTMLInputElement;
@@ -34,8 +35,8 @@ function enviarMensaje(texto: string) {
   //parmensaje.innerText = texto;
 }
 //-----------------------------------------------------------------------------
-function enableBtn(enable: boolean){
-  btnSend.disabled = enable; //habilitar boton 
+function enableBtn(enable: boolean) {
+  btnSend.disabled = !enable; //habilitar boton 
   if (enable) {
     if (btnSend.classList.contains("disabled")) btnSend.classList.remove("disabled")
   } else {
@@ -43,9 +44,9 @@ function enableBtn(enable: boolean){
   }
 }
 //-----------------------------------------------------------------------------
-function isValidText(texto: string): boolean {
+function isValidText(texto: string, max: number = 50): boolean {
   //  console.log({texto});  
-  return (texto.length >= 3 && texto.length <= 50);
+  return (texto.length >= 3 && texto.length <= max);
 }
 //-----------------------------------------------------------------------------
 function isValidEmail(texto: string): boolean {
@@ -64,7 +65,7 @@ function isValidEmail(texto: string): boolean {
 function isValidPhone(texto: string): boolean {
   //console.log({ texto });
   const emailRegex: RegExp = /^[0-9- \(\+)]{0,63}$/;
-  return (texto.length > 5 && emailRegex.test(texto) );
+  return (texto.length > 5 && emailRegex.test(texto));
 }
 //-----------------------------------------------------------------------------
 function isFormComplete(): boolean {
@@ -73,14 +74,14 @@ function isFormComplete(): boolean {
     && isValidEmail(inputEmail.value)
     && isValidPhone(inputTelefono.value)
     && isValidText(inputLocalidad.value)
-    && isValidText(inputComentario.value));
+    && isValidText(inputComentario.value, maxComentario));
   //console.log({isComplete});
-  
-/*  if (isComplete) {
-    if (btnSend.classList.contains("disabled")) btnSend.classList.remove("disabled")
-  } else {
-    if (!btnSend.classList.contains("disabled")) btnSend.classList.add("disabled");
-  }*/
+
+  /*  if (isComplete) {
+      if (btnSend.classList.contains("disabled")) btnSend.classList.remove("disabled")
+    } else {
+      if (!btnSend.classList.contains("disabled")) btnSend.classList.add("disabled");
+    }*/
   return isComplete;
   //  return isValid() && isValidNombre() && isValidTelefono() && isValidEmail() && isValidLocalidad() && isValidComentario() 
 }
@@ -88,89 +89,86 @@ function isFormComplete(): boolean {
 //-----------------------------------------------------------------------------
 inputNombre.addEventListener("input", () => {
   let nombre: string = String(inputNombre.value);
-//  btnSend.disabled = true;
+  //  btnSend.disabled = true;
   if (!isValidText(nombre)) {
     enableBtn(false);
-    enviarMensaje("Ingrese su nombre.");
+    enviarMensaje("Ingrese su nombre. (entre 3 y 50 caracteres)");
     return
   }
   enableBtn(isFormComplete());
-//  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
+  //  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
 });
 //-----------------------------------------------------------------------------
 inputEmpresa.addEventListener("input", () => {
   let emmpresa: string = String(inputEmpresa.value);
-//  btnSend.disabled = true;
+  //  btnSend.disabled = true;
   if (!isValidText(emmpresa)) {
     enableBtn(false);
-    enviarMensaje("Ingrese el nombre de su empresa.");
+    enviarMensaje("Ingrese el nombre de su empresa. (entre 3 y 50 caracteres)");
     return
   }
   enableBtn(isFormComplete());
-//  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
+  //  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
 });
 //-----------------------------------------------------------------------------
 inputEmail.addEventListener("input", () => {
   let email: string = String(inputEmail.value);
-//  btnSend.disabled = true;
+  //  btnSend.disabled = true;
   if (!isValidEmail(email)) {
     enableBtn(false);
     enviarMensaje("Ingrese un correo electrónico válido.");
     return
   }
   enableBtn(isFormComplete());
-//  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
+  //  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
 });
 //-----------------------------------------------------------------------------
 inputTelefono.addEventListener("input", () => {
   //  inputEmail = event.target;
   let phone: string = String(inputTelefono.value);
-//  btnSend.disabled = true;
+  //  btnSend.disabled = true;
   if (!isValidPhone(phone)) {
     enableBtn(false);
-    enviarMensaje("Ingrese un número de telefono.");  //throw new Error(    'No es un nro de telefono valido')
+    enviarMensaje("Ingrese un número de telefono válido. (minimo 6 digitos)");  //throw new Error(    'No es un nro de telefono valido')
     return
   }
   enableBtn(isFormComplete());
-//  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
+  //  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
 });
 //-----------------------------------------------------------------------------
 inputLocalidad.addEventListener("input", () => {
   let localidad: string = String(inputLocalidad.value);
-//  btnSend.disabled = true;
+  //  btnSend.disabled = true;
   if (!isValidText(localidad)) {
     enableBtn(false);
-    enviarMensaje("Ingrese el nombre de su Localidad.");
+    enviarMensaje("Ingrese el nombre de su Localidad. (entre 3 y 50 caracteres)");
     return
   }
   enableBtn(isFormComplete());
-//  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
+  //  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
 });
 //-----------------------------------------------------------------------------
 inputComentario.addEventListener("input", () => {
   let comentario: string = String(inputComentario.value);
-//  btnSend.disabled = true;
-  if (!isValidText(comentario)) {
+  //  btnSend.disabled = true;
+  if (!isValidText(comentario, maxComentario)) {
     enableBtn(false);
-    enviarMensaje("Ingrese una breve descripción del problema.");
+    enviarMensaje("Ingrese una breve descripción del problema. (entre 3 y 250 caracteres)");
     return
   }
   enableBtn(isFormComplete());
-//  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
+  //  if (isFormComplete()) btnSend.disabled = false; //habilitar boton 
 });
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 btnSend.addEventListener("click", () => {
-  // validar. no ya viene validdado.
-  // let dato: string = inputNombre.value;
-  // let email: string = inputEmail.value;
-//  alert("Datos enviados con éxito. En breve nos contactaremos. Muchas gracias por consultarnos.");
+  // validar. no ya viene validado.
+
+  //  alert("Datos enviados con éxito. En breve nos contactaremos. Muchas gracias por consultarnos.");
   console.log("Datos enviados con éxito. En breve nos contactaremos. Muchas gracias por consultarnos.");
 
   // limpiar el form.
-  //    btnSend.hidden = true;  
-  //btnSend.disabled = true;
-
+  enableBtn(false);
   inputNombre.value = "";
   inputEmpresa.value = "";
   inputEmail.value = "";
